f07792503d7abbef17bed7e46cf1469862f50c3f
Reahana <reahana233@gmail.com>
2023-03-31 18:24:29 +0600

    template string

---
+++ js/template.js
@@ -0,0 +1,16 @@
+const person = 'Asrafi';
+const person2 = 'Reahana';
+const person3 = `React`;
+
+const multilineOld = 'I love React \n'+
+'JavaScript is fun \n'+
+'happy codding  \n'
+
+const multiline = `I love React
+JavaScript is fun 
+happy coding
+`
+const a =1;
+const b =4;
+const number = `sum ${a} and ${b} is ${a+b}`
+console.log(number );
