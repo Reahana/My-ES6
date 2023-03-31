007fb21b37a41ef2824471e7dc40b3ae1ec70874
Reahana <reahana233@gmail.com>
2023-03-31 18:53:08 +0600

    more arrow function

---
+++ js/more-arrow.js
@@ -0,0 +1,12 @@
+const add = (a,b)=>a+b;
+const addAll = (a,b,c,d,e,f)=>a+b+c+d+e+f;
+const fullname = (f,l) => f+''+l
+const pie = ()=> 3.14;
+const single = num => num*5;
+const multiLineArrow = (x,y,z)=>{
+    const multifly=x*y ;
+    const sub = y-z;
+    const sum = multifly +sub
+    return sum;
+
+}
