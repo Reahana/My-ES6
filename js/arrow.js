401594b58b9ae58c05a8a763475cadc54879d1d0
Reahana <reahana233@gmail.com>
2023-03-31 18:32:46 +0600

    arrow function

---
+++ js/arrow.js
@@ -0,0 +1,26 @@
+function add0 (a,b){
+    const c = a+b;
+    return c;
+}
+//function expression
+const add1 = function add (a,b){
+    const c = a+b;
+    return c;
+}
+const add2 = function  (a,b){
+    const c = a+b;
+    return c;
+}
+
+function add4 (a,b){
+    return a+b;
+     
+}
+
+const add5 = function(a,b){
+    return a+b;
+     
+}
+const add = (a,b)=>a+b;
+const result = add (5,3);
+console.log(result);
