fa4b085aed65e510cd3a3f49d9132fc038eb7d53
Reahana <reahana233@gmail.com>
2023-03-31 18:14:54 +0600

    default parameter

--- js/default.js
+++ js/default.js
@@ -1,5 +1,20 @@
-function add(a,b){
-    const total =a+b;
+// function add(a,b){
+//     b = b || 0;
+//     if(b === undefined){
+//         b =0;
+//     }
+//     const total =a+b;
+//     return total;
+// }
+
+function add(a, b=0){
+    const total = a+b;
     return total;
 }
-add(10,20);
+const result = add(10,20);
+console.log(result);
+
+
+// a, b parameter
+// a , b =0 default parameter (call er time e man na dile ata use hobe)
+// 10, 20 argument 
