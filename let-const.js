b24e1aa8f31505d188d2440f4d38cf413e85a180
Reahana <reahana233@gmail.com>
2023-03-31 18:05:44 +0600

    let const

---
+++ let-const.js
@@ -0,0 +1,14 @@
+let money = 25;
+money = 35;
+
+const bird = 'crow';
+const numbers = [45,67,34];
+//const reassign is not allow
+
+const student ={
+    name:'reahana',
+    age:25
+}
+
+
+//{} er moddhe theke baire jete parbe na const & let => block scope
